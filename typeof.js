779245ab08a897d38426d539caf3a747b4ad5a6d
Christian Broberg <cb@webhouse.dk>
2017-08-17 23:47:27 +0200

    Added typeof

---
+++ typeof.js
@@ -0,0 +1,18 @@
+const cls = require('./services/lib').cls
+const log = require('./services/lib').log
+
+cls()
+
+const someString = 'This is a string'
+const someNumber = 5
+
+if (typeof (log(cls())) === 'undefined') { // undefined
+	log('UNDEFINED ... and now to something else!')
+}  
+
+log(typeof(someString))
+
+if (typeof(someNumber) === 'number') {
+	log(typeof(someNumber))
+}
+
