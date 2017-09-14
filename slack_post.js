22096a8c082cf0cd72aaa613182916f2de81e5ab
Christian Broberg <cb@webhouse.dk>
2017-09-15 00:47:28 +0200

    Update data

--- slack_post.js
+++ slack_post.js
@@ -1,46 +1,19 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
-
-/* 
-const slackURL = 'https://hooks.slack.com/services/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'
-const message = '{"text": "Send from NodeJS to ODEUM Code Slack channel!"}'
-
-var xhr = new XMLHttpRequest()
-
-xhr.open("POST", slackURL, true)
-
-//Send the proper header information along with the request
-xhr.setRequestHeader('Content-type', 'application/json')
-
-xhr.onreadystatechange = function () {//Call a function when the state changes.
-	if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-		xhr.send(message)
-	}
-}
- */
-
-// xhr.send(message)
-// xhr.send('string'); 
-// xhr.send(new Blob()); 
-// xhr.send(new Int8Array()); 
-// xhr.send({ form: 'data' }); 
-// xhr.send(document);
+const channels = require('./slack_data').channels
+const buttons = require('./slack_data').buttons
+const book = require('./slack_data').book
+const json = require('./slack_data').json
 
 
 
-/*
-curl -X POST -H 'Content-type: application/json' --data '{"text":"This is send from ODEUM Code!!!"}' https://hooks.slack.com/services/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
-
-*/
-
-
-function postMessageToSlack() {
-	var xmlhttp = new XMLHttpRequest(),
-		webhook_url = 'https://hooks.slack.com/services/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',
-		myJSONStr = '{"text": "Send from NodeJS to ODEUM Code Slack channel!"}'
-
-	xmlhttp.open('POST', webhook_url, false)
+function postMessageToSlack(url, message) {
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
+	var xmlhttp = new XMLHttpRequest()
+	xmlhttp.open('POST', url, false)
 	xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-	xmlhttp.send(myJSONStr)
+	xmlhttp.send(message)
 }
 
-postMessageToSlack()
+// console.log(channels['cb'].value, channels['cb'].message)
+
+postMessageToSlack(channels['cb'].value, book)
+
