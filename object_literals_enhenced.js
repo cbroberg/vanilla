d710601f79ecbb94aad2af6125bdc7da541ac93a
Christian Broberg <cb@webhouse.dk>
2017-10-01 17:38:51 +0200

    Enhanced object literals

---
+++ object_literals_enhenced.js
@@ -0,0 +1,22 @@
+const cls = require('./services/lib').cls
+const log = require('./services/lib').log
+
+cls()
+
+
+const color = 'red'
+const point = {
+	x: 10,
+	y: 20,
+	color,
+	toString() {
+		return 'x=' + this.x + ', y=' + this.y + ', color=' + this.color
+	},
+	['prop_' + 42]: 42
+}
+
+let p = point.toString()
+
+log(p)
+log('The dynamic property is ' + point.prop_42)
+
