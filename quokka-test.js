28ccead4dc9ee81be6a99522e1a58980f2b1e9e0
Christian Broberg <cb@webhouse.dk>
2018-10-19 14:44:27 +0200

    Backup

--- quokka-test.js
+++ quokka-test.js
@@ -1,19 +1,19 @@
 const fetch = require('node-fetch')
 
-/* function fetchCatAvatars(userId) {
-	return fetch(`https://catappapi.herokuapp.com/users/${userId}`)
-		.then(response => response.json())
-		.then(user => {
-			const promises = user.cats.map(catId =>
-				fetch(`https://catappapi.herokuapp.com/cats/${catId}`)
-					.then(response => response.json())
-					.then(catData => catData.imageUrl))
-			return Promise.all(promises)
-		})
+// Uses Promise.all() = parallel 
+const fetchCatAvatars1 = async (userId) => {
+	const response = await fetch(`http://catappapi.herokuapp.com/users/${userId}`)
+	const user = await response.json()
+	user 
+	return await Promise.all(user.cats.map(async (catId) => {
+		const response = await fetch(`http://catappapi.herokuapp.com/cats/${catId}`)
+		const catData = await response.json()
+		return catData.imageUrl
+	}))
 }
- */
 
-async function fetchCatAvatars(userId) {
+// Uses Await and synchronous loop = synchronous 
+const fetchCatAvatars2 = async (userId) => {
 	const response = await fetch(`http://catappapi.herokuapp.com/users/${userId}`)
 	const user = await response.json()  
 	const catImageUrls = []
@@ -25,13 +25,20 @@ async function fetchCatAvatars(userId) {
 	return catImageUrls
 }
 
-const result = fetchCatAvatars(123)
+const result1 = fetchCatAvatars1(123)
+const result2 = fetchCatAvatars2(123)
+// result1 
+// result2 
 
-result.then((res) => {
-	// console.log(res)
+result1.then((res) => {
+	console.log('Promise.all')
 	res.map(url => console.log(url) 
 	) 	
 } )
 
+result2.then((res) => {
+	console.log('Await')
+	res.map(url => console.log(url) 
+	) 	
+} )
 
-// result 
