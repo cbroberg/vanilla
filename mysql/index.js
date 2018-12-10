9ef41e07360652f8d748bcb6f3b1b87b5b867378
Christian Broberg <cb@cb-pro.local>
2018-12-10 16:57:09 +0100

    MySQL is running locally + on server

--- mysql/index.js
+++ mysql/index.js
@@ -3,11 +3,11 @@ require('dotenv').load()
 const mysql = require('mysql2')
 // const mysql = require('mysql')
 
-const { DB_HOST, DB_USER, DATABASE, PASSWORD } = process.env
+const { DB_HOST, DB_HOST_LOCAL, DB_USER, DATABASE, PASSWORD } = process.env
 
 // create the connection to database
 const connection = mysql.createConnection({
-	host: DB_HOST,
+	host: DB_HOST_LOCAL,
 	user: DB_USER,
 	password: PASSWORD,
 	database: DATABASE
@@ -15,12 +15,24 @@ const connection = mysql.createConnection({
 
 // console.log(connection)
 
-// simple query
+// simple query 1
 connection.query(
 	'SELECT * FROM `events` WHERE `name` = "Christian Broberg"',
 	function (err, results, fields) {
 		console.log(err)
 		console.log(results) // results contains rows returned by server
-		console.log(fields) // fields contains extra meta data about results, if available
+		// console.log(fields) // fields contains extra meta data about results, if available
 	}
 )
+
+connection.query(
+	'SELECT * FROM `events` WHERE `id` = "1"',
+	function (err, results, fields) {
+		console.log(err)
+		console.log(results) // results contains rows returned by server
+		// console.log(fields) // fields contains extra meta data about results, if available
+	}
+)
+
+
+connection.end()
